89cf6e664d74529fbc449d37090948ac0e7af206
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-08 21:34:46 +0300

    2

---
+++ number1.py
@@ -0,0 +1,11 @@
+__author__ = 'student'
+import numpy as np
+x=float(input())
+y=(np.log((np.exp(1/(np.sin(x)+1))/(5/4+1/(x**15)))))/(np.log(1+x**2))
+print (y)
+x=float(input())
+y=(np.log((np.exp(1/(np.sin(x)+1))/(5/4+1/(x**15)))))/(np.log(1+x**2))
+print (y)
+x=float(input())
+y=(np.log((np.exp(1/(np.sin(x)+1))/(5/4+1/(x**15)))))/(np.log(1+x**2))
+print (y)
