51722347cd0833cc6c22f341558a247b141f8912
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-08 21:34:30 +0300

    2

--- plot6.py
+++ plot6.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 a=10
 b=0.5
-x=np.arange(0,7,0.001)
+x=np.arange(0,7,0.01)
 n=0
 y=0
-while n<100:
+while n<300:
     y+=b**n*np.cos(a**n*np.pi*x)
     n+=1
 
