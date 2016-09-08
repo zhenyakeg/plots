775cfe1c31235da2fed5677d33d0497d8fdc8ba3
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-09-08 21:32:13 +0300

    2

--- plot6.py
+++ plot6.py
@@ -1,1 +1,14 @@
 __author__ = 'student'
+import numpy as np
+import matplotlib.pyplot as plt
+a=10
+b=0.5
+x=np.arange(0,7,0.001)
+n=0
+y=0
+while n<100:
+    y+=b**n*np.cos(a**n*np.pi*x)
+    n+=1
+
+plt.plot(x,y)
+plt.show()
